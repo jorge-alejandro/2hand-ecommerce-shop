8cbffe53ea78b0a52f4ef15e0cab91c9c47b4bfa
Jorge Cidre Fernández <126116167+Lithuniel@users.noreply.github.com>
2026-05-02 17:38:01 +0200

    Add '/init-db' endpoint for database initialization
    Added a new endpoint '/init-db' to initialize the database and handle errors during the process.

--- 2hand-ecommerce-shop-back/src/app.js
+++ 2hand-ecommerce-shop-back/src/app.js
@@ -5,13 +5,11 @@ const indexRouter = require("./routes/index-router");
 const validateToken = require("./middlewares/validate-token");
 const sendErrorResponse = require("./utils/send-errors-response");
 const path = require("path");
-
 const app = express();
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log("Server iniciado en el puerto " + PORT);
 });
-
 app.use(
   cors({
     origin: [
@@ -22,18 +20,25 @@ app.use(
   })
 );
 
+app.get("/init-db", async (req, res) => {
+  try {
+    const initDb = require("./database/db-init");
+    await initDb();
+    res.send("Base de datos inicializada correctamente ✅");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error al inicializar la base de datos: " + err.message);
+  }
+});
+
 app.use(validateToken);
-
 app.use(indexRouter);
-
 const staticDirectory = path.join(__dirname, "../public");
 app.use(express.static(staticDirectory));
-
 app.use((err, req, res, next) => {
   console.error(err);
   sendErrorResponse(res, err);
 });
-
 app.use((req, res, next) => {
   sendErrorResponse(res, {
     status: 404,
